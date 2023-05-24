dac3539f6c0552cf4bd43fe477f0564e3d15f7b1
RazziTechverx <raazi@gems.techverx.com>
2023-05-24 13:31:42 +0200

    Changes in index file

--- index.php
+++ index.php
@@ -7,43 +7,44 @@ require_once "src/Helpers/Helper.php";
 use src\Helpers\Helper;
 use src\Starship;
 
-// Fetch starship data from SWAPI API
-$response = Helper::curlAPI(Helper::API_URL);
-$data = json_decode($response, true);
+try {
+    // Fetch starship data from SWAPI API
+    $response = Helper::curlAPI(Helper::API_URL);
+    $data = json_decode($response, true);
 
-if (!empty($data['results'])) {
-    $starships = $data['results'];
+    if (!empty($data['results'])) {
+        $starships = $data['results'];
 
-    $starshipObjects = [];
+        $starshipObjects = [];
 
-    foreach ($starships as $starship) {
-        $pilots = [];
+        foreach ($starships as $starship) {
+            $pilots = [];
 
-        foreach ($starship['pilots'] as $pilotUrl) {
-            $pilotData = json_decode(Helper::curlAPI($pilotUrl), true);
-            $pilots[] = [
-                'name' => $pilotData['name'],
-                'height' => $pilotData['height']
-            ];
+            foreach ($starship['pilots'] as $pilotUrl) {
+                $pilotData = json_decode(Helper::curlAPI($pilotUrl), true);
+                $pilots[] = [
+                    'name' => $pilotData['name'],
+                    'height' => $pilotData['height']
+                ];
+            }
+
+            $starshipObjects[] = new Starship(
+                $starship['name'],
+                $starship['model'],
+                $starship['cargo_capacity'],
+                (int)$starship['max_atmosphering_speed'],
+                $starship['crew'],
+                $pilots
+            );
         }
 
-        $starshipObjects[] = new Starship(
-            $starship['name'],
-            $starship['model'],
-            $starship['cargo_capacity'],
-            (int)$starship['max_atmosphering_speed'],
-            $starship['crew'],
-            $pilots
-        );
-    }
-
 // Sort Starships by fastest (descending order)
-    usort($starshipObjects, function ($a, $b) {
-        return $b->getSpeed() - $a->getSpeed();
-    });
+        usort($starshipObjects, function ($a, $b) {
+            return $b->getSpeed() - $a->getSpeed();
+        });
 
 // Display the Starships
-    echo "<table>
+        echo "<table>
         <tr>
             <th>Name</th>
             <th>Model</th>
@@ -53,33 +54,37 @@ if (!empty($data['results'])) {
             <th>Speed Difference</th>
         </tr>";
 
-    $fastestStarship = $starshipObjects[0]->getSpeed();
+        $fastestStarship = $starshipObjects[0]->getSpeed();
 
-    foreach ($starshipObjects as $starship) {
-        $speedDifference = abs(round(($starship->getSpeed() - $fastestStarship) / $fastestStarship * 100));
+        foreach ($starshipObjects as $starship) {
+            $speedDifference = abs(round(($starship->getSpeed() - $fastestStarship) / $fastestStarship * 100));
 
-        echo "<tr>
+            echo "<tr>
             <td>{$starship->getName()}</td>
             <td>{$starship->getModel()}</td>
             <td>{$starship->getCargoCapacity()}</td>
             <td>";
 
-        if (!empty($starship->getPilots())) {
-            foreach ($starship->getPilots() as $pilot) {
-                echo "{$pilot['name']} (Height: {$pilot['height']})<br>";
+            if (!empty($starship->getPilots())) {
+                foreach ($starship->getPilots() as $pilot) {
+                    echo "{$pilot['name']} (Height: {$pilot['height']})<br>";
+                }
+            } else {
+                echo "Unknown";
             }
-        } else {
-            echo "Unknown";
-        }
 
-        echo "</td>
+            echo "</td>
             <td>{$starship->getCrewSize()}</td>
             <td>{$speedDifference}% slower</td>
           </tr>";
-    }
+        }
 
-    echo "</table>";
-} else {
-    echo Helper::NOT_FOUND;
+        echo "</table>";
+    } else {
+        echo Helper::NOT_FOUND;
+        die();
+    }
+} catch (Exception $e) {
+    echo 'Exception Message: ' . $e->getMessage();
     die();
 }
